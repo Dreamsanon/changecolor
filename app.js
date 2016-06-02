ce7899fd9ed1c468518e7de223db23f71abf5214
Dylan Dreams <Dylan Dreams>
2016-06-02 20:07:38 +0200

    Add App.js including and fix Jquery selector

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	$('#changeColorRed').on('click', function(){
-		$(this).css('color', 'red');
+		$('#colored').css('color', 'red');
 	})
 
 	$('#changeColorGreen').on('click', function(){
-		$(this).css('color', 'green');
+		$('#colored').css('color', 'green');
 	})
 });
