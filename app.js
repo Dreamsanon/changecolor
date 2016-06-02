655c0f74cb5c254ea6b3942da8ab5007f2b2e5ee
Dylan Dreams <Dylan Dreams>
2016-06-02 20:05:38 +0200

    Add Main javascript code and functionality

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	
+	$('#changeColorRed').on('click', function(){
+		$(this).css('color', 'red');
+	})
+
+	$('#changeColorGreen').on('click', function(){
+		$(this).css('color', 'green');
+	})
 });
